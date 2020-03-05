b2c1371990ae57481a4dff6cd780c7c14ec7d5a8
dd <dd@greatech.ru>
2020-03-05 23:13:29 +0400

    Обновил

--- src/main/java/Yurchenko/Service.java
+++ src/main/java/Yurchenko/Service.java
@@ -2,50 +2,40 @@ package Yurchenko;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-    public class Service<T extends Human> {
+public class Service {
 
-        private String stringGen() {
-            String string = "abcdefghijklmnop";
-            StringBuilder randString = new StringBuilder();
-            int count = (int) (Math.random() * 8 + 2);
-            for (int i = 0; i < count; i++)
-                randString.append(string.charAt((int) (Math.random() * string.length())));
-            return randString.toString();
+    public HumanDTO get(HumanDTO dto) {
+        Converter converter = new Converter();
+        Repository repository = new Repository();
+        Human human = converter.ConvertToEntity(dto);
+        repository.get(human);
+        dto = converter.ConvertToDTO(human);
+        return dto;
+    }
+
+    public List<HumanDTO> getAll(int i) {
+        List<HumanDTO> list = new ArrayList<>();
+        for (int j = 0; j < i; j++) {
+            HumanDTO dto = new HumanDTO();
+            list.add(get(dto));
         }
+        return list;
+    }
 
-
-        Human get(Human human) {
-            Human.Address address = new Human(). new Address();
-            human.setId((int) (Math.random() * 10));
-            human.setName(stringGen());
-            human.setBirthDate(new Date((long) (Math.random() * 100000000)));
-            address.setCity(stringGen());
-            address.setStreet(stringGen());
-            address.setHouseNumber((int) (Math.random() * 100));
-            human.setAddress(address);
-            return human;
-        }
-
-
-        List<Human> getAll(int i) {
-            Service ser = new Service();
-            List<Human> list = new ArrayList<>();
-            for (int j = 0; j < i; j++) {
-                Human human = new Human();
-                list.add(ser.get(human));
-            }
-            return list;
-        }
-
-        void save(Human human) {
-            System.out.println(human);
-        }
-
-        void saveAll(List<Human> people) {
-            for (Human human : people) {
-                System.out.println(human);
-            }
+    public void saveAll(List<HumanDTO> alldto) {
+        for (HumanDTO dto : alldto) {
+            Converter converter = new Converter();
+            Repository repository = new Repository();
+            Human human = converter.ConvertToEntity(dto);
+            repository.save(human);
         }
     }
+}
+
