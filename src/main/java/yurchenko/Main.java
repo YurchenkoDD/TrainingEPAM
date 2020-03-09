9a7f9c70698368f46da72ad6122d306fc41921a7
dd <dd@greatech.ru>
2020-03-09 16:47:11 +0400

    MAin

--- src/main/java/yurchenko/Main.java
+++ src/main/java/yurchenko/Main.java
@@ -1,7 +1,23 @@
 package yurchenko;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        System.out.print("Hello, world!");
+        Tasks task = new Tasks();
+        List<Human> humansList = task.fillList();
+        List<User> users = task.fillUsers();
+        HashMap<Integer, String> footballers = task.fillMap();
+        task.printCollection(humansList);
+        task.findDoubles(humansList);
+        task.removeDoubles(humansList);
+        task.sortByFio(humansList);
+        task.sortByAge(humansList);
+        task.sortByAddress(humansList);
+        task.greetUsers(users);
+        task.sortByKey(footballers);
+        task.sortByValue(footballers);
+        task.randLinkList();
     }
 }
