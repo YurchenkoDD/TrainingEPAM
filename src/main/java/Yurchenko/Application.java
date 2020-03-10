313af5550a56bf930e5a0841790e8019cc691832
dd <dd@greatech.ru>
2020-03-10 16:30:47 +0400

    Проверил slf4j

--- src/main/java/Yurchenko/Application.java
+++ src/main/java/Yurchenko/Application.java
@@ -1,11 +1,17 @@
 package Yurchenko;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+
         Service ser = new Service();
         HumanDTO alldto = new HumanDTO();
+        Converter converter = new Converter();
+        Human human = converter.ConvertToEntity(alldto);
         ser.get(alldto);
         List<HumanDTO> humanDTOS = ser.getAll(5);
         ser.saveAll(humanDTOS);
