68208f94e4b8cde76b1e51337f42aca2d3db4274
dd <dd@greatech.ru>
2020-03-12 17:03:56 +0400

    Добавил непроверяемое исключение

---
+++ src/main/java/Yurchenko/exceptions/CanNotSaveEntity.java
@@ -0,0 +1,8 @@
+package Yurchenko.exceptions;
+
+//Unchecked exception
+public class CanNotSaveEntity extends RuntimeException {
+    public CanNotSaveEntity(String message){
+        super(message);
+    }
+}
