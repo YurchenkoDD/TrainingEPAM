0f784e9da95528d06fca524a6217d52c8c6d25c1
dd <dd@greatech.ru>
2020-03-12 17:03:24 +0400

    Реализовал исключения

--- src/main/java/Yurchenko/Application.java
+++ src/main/java/Yurchenko/Application.java
@@ -1,17 +1,15 @@
 package Yurchenko;
 
+import Yurchenko.exceptions.EntityNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws EntityNotFound {
         Service ser = new Service();
         HumanDTO alldto = new HumanDTO();
-        Converter converter = new Converter();
-        Human human = converter.ConvertToEntity(alldto);
         ser.get(alldto);
         List<HumanDTO> humanDTOS = ser.getAll(5);
         ser.saveAll(humanDTOS);
