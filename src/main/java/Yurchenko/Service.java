0446c77d542a72dca094ec57618bf014e75df7db
dd <dd@greatech.ru>
2020-03-12 17:05:45 +0400

    Реализовал исключения и логгирование

--- src/main/java/Yurchenko/Service.java
+++ src/main/java/Yurchenko/Service.java
@@ -1,41 +1,51 @@
 package Yurchenko;
 
-import java.util.List;
-import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Date;
+import Yurchenko.exceptions.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Service {
+    private Converter converter = new Converter();
+    private Repository repository = new Repository();
+    private Logger fileLogger = LoggerFactory.getLogger(Service.class);
+    private static final int AllOWED_ID = 30;
 
     public HumanDTO get(HumanDTO dto) {
-        Converter converter = new Converter();
-        Repository repository = new Repository();
         Human human = converter.ConvertToEntity(dto);
-        repository.get(human);
-        dto = converter.ConvertToDTO(human);
-        return dto;
+        try{
+            repository.get(human);
+            fileLogger.info("Сущность с id = {} и name = {} получена из БД", dto.getId(), dto.getName());
+        }catch(CanNotSaveEntity e){
+            fileLogger.warn("Exception: ",e);
+        }
+        return converter.ConvertToDTO(human);
     }
 
-    public List<HumanDTO> getAll(int i) {
+    public List<HumanDTO> getAll(int i) throws EntityNotFound {
         List<HumanDTO> list = new ArrayList<>();
         for (int j = 0; j < i; j++) {
             HumanDTO dto = new HumanDTO();
+            if(dto.getId()>AllOWED_ID){
+                fileLogger.warn("Человек с id = {} и name = {} не найден", dto.getId());
+                throw new EntityNotFound("Человек с id"+ dto.getId()+" не найден");
+            }
             list.add(get(dto));
         }
         return list;
     }
 
-    public void saveAll(List<HumanDTO> alldto) {
-        for (HumanDTO dto : alldto) {
-            Converter converter = new Converter();
-            Repository repository = new Repository();
-            Human human = converter.ConvertToEntity(dto);
-            repository.save(human);
+    public void saveAll(List<HumanDTO> dtos) {
+        for (HumanDTO dto : dtos) {
+            Human human=converter.ConvertToEntity(dto);
+            try {
+                repository.save(human);
+                fileLogger.info("Сущность с id = {} и name = {} сохранена в БД", dto.getId(), dto.getName());
+            } catch (CanNotSaveEntity e){
+                fileLogger.warn("Exception: ",e);
+            }
         }
     }
 }
-
