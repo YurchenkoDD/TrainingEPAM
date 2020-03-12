3025935c7f28f6083c796ace95eac6e0e21c87fe
dd <dd@greatech.ru>
2020-03-12 17:05:37 +0400

    Реализовал исключения и логгирование

--- src/main/java/Yurchenko/Repository.java
+++ src/main/java/Yurchenko/Repository.java
@@ -1,13 +1,20 @@
 package Yurchenko;
 
+import Yurchenko.exceptions.CanNotSaveEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Date;
 
 public class Repository<T extends Human> {
 
+    private Logger consoleLogger = LoggerFactory.getLogger(Service.class);
+    private static final int BOOKED_ID = 2;
+
     private String stringGen() {
         String string = "abcdefghty";
         StringBuilder randString = new StringBuilder();
-        int count = (int) (Math.random() * 9 + 3);
+        int count = (int) (Math.random() * 8 + 2);
         for (int i = 0; i < count; i++)
             randString.append(string.charAt((int) (Math.random() * string.length())));
         return randString.toString();
@@ -15,7 +22,7 @@ public class Repository<T extends Human> {
 
     public Human get(Human human) {
         Human.Address address = new Human().new Address();
-        human.setId((int) (Math.random() * 10));
+        human.setId((int) (Math.random() * 15 + 1));
         human.setName(stringGen());
         human.setBirthDate(new Date((long) (Math.random() * 100000)));
         address.setCity(stringGen());
@@ -26,6 +33,11 @@ public class Repository<T extends Human> {
     }
 
     public void save(Human human) {
+        if(human.getId()<=BOOKED_ID){
+            consoleLogger.warn("Человек с id = {} не может быть сохранен в БД. Этот ID занят",human.getId());
+            throw new CanNotSaveEntity("Человек с id="+human.getId()+" не может быть сохранен в БД. Этот ID занят");
+        }
         System.out.println(human);
+        consoleLogger.info("Человек с id = {} сохранен", human.getId());
     }
 }
