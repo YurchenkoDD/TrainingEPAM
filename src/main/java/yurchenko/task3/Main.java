188db395dc711f4dcedd968fbf44d9bb90745266
ddmi1553 <dimanych112@mail.ru>
2020-03-22 21:08:56 +0400

    Загрузчик класса

---
+++ src/main/java/yurchenko/task3/Main.java
@@ -0,0 +1,20 @@
+package yurchenko.task3;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        // Путь указан для систем на базе Unix
+        CustomClassLoader classLoader = new CustomClassLoader("/home/dd/workflowLocal/");
+        Class<?> clazz;
+        try {
+            clazz = classLoader.loadClass("TestClass");
+            log.info("Loaded " + clazz.getName() + ".class");
+            Object obj = clazz.newInstance();
+            System.out.println(obj);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
