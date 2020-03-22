630bf2fccd689be818d0f275e83abdcb520d6744
ddmi1553 <dimanych112@mail.ru>
2020-03-22 21:07:17 +0400

    Загрузка класса

---
+++ src/main/java/yurchenko/task3/CustomClassLoader.java
@@ -0,0 +1,46 @@
+package yurchenko.task3;
+
+import lombok.extern.slf4j.Slf4j;
+import java.io.*;
+
+@Slf4j
+public class CustomClassLoader extends ClassLoader {
+
+    private String pathToBin;
+
+    public CustomClassLoader(String path){
+        super();
+        pathToBin = path;
+    }
+
+    @Override
+    public Class<?> findClass(String className) throws ClassNotFoundException {
+        try {
+            byte[] bytes = fetchClassFromFS(pathToBin + className + ".class");
+            return defineClass(className, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return super.findClass(className);
+        }
+    }
+
+    private byte[] fetchClassFromFS(String path) throws IOException {
+        InputStream inputStream = new FileInputStream(new File(path));
+        long length = new File(path).length();
+        if (length > Integer.MAX_VALUE) {
+            throw new IOException("File is too large");
+        }
+        byte[] bytes = new byte[(int) length];
+        int offset = 0;
+        int numRead;
+        while (offset < bytes.length
+                && (numRead = inputStream.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + path);
+        }
+        inputStream.close();
+        return bytes;
+    }
+}
