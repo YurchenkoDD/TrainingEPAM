2be76d80c8b3dec7c8852eaaffca0e9ac4fbc0cc
ddmi1553 <dimanych112@mail.ru>
2020-03-26 22:12:53 +0400

    refactor

--- src/main/java/yurchenko/task2/Main.java
+++ src/main/java/yurchenko/task2/Main.java
@@ -1,5 +1,11 @@
 package yurchenko.task2;
 
+/**
+ * Simple class to demonstrate StackOverflowError
+ * That error will causes by calling
+ * recursive method that overflow the stack
+ */
+
 public class Main {
     public static void main(String[] args) {
         int counter = 0;
